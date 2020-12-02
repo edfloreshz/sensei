25c6ee6e2f968f8747f3b9b6360f7bce5df4d315
Eduardo Flores <edfloreshz@gmail.com>
2020-12-02 11:19:57 -0700

    v0.1.4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 use webbrowser;
+use rand::Rng;
+
+struct CrateInfo {
+    name: String,
+    version: String
+}
+
+const PHRASES: [&str; 3] = ["幸運を","よく学ぶ","良い読書"];
 
 /// Receives input from the user to process their request.
     ///
@@ -19,26 +27,52 @@ fn main() {
         .author("Eduardo F. <edfloreshz@gmail.com>")
         .about("Opens the documentation for any crate.")
         .arg(Arg::with_name("crate")
-            .help("What crate do you need help with, gakusei?")
+            .help("What crate do you need help with, 学生?")
             .required(true)
             .index(1))
-        .arg(Arg::with_name("v")
-            .value_name("version")
-            .help("Specify version for a crate")
-            .takes_value(true))
+        .subcommand(SubCommand::with_name("-v")
+            .help("Flag used to specify a crate's version.")
+            .arg(Arg::with_name("ver")
+                .help("ver")
+                .takes_value(true)))
         .get_matches();
-    let crt = matches.value_of("crate").unwrap();
-    match matches.value_of("v") {
-        Some(v) => open_url(format!("https://docs.rs/{}/{}/{}", crt, v, crt), crt),
-        None => open_url(format!("https://docs.rs/{}", crt), crt)
+
+    let mut crt = CrateInfo {
+        name: matches.value_of("crate").unwrap().into(),
+        version: "Latest version".into()
+    };
+    if let Some(matches) = matches.subcommand_matches("-v") {
+        if matches.is_present("ver") {
+            crt.version = matches.value_of("ver").unwrap().parse().unwrap();
+            open_url(
+                format!("https://docs.rs/{}/{}/{}", crt.name, crt.version, crt.name),
+                crt
+            )
+        } else {
+            println!("Please specify a version for this crate.");
+        }
+    } else {
+        open_url(format!("https://docs.rs/{}", crt.name), crt)
     }
 }
 
 /// Opens the requested crate's documentation in the web browser.
-fn open_url(url: String, crt: &str) {
+fn open_url(url: String, crt: CrateInfo) {
     if webbrowser::open(&*url).is_ok() {
-        println!("Here it is... the book of {}. よく学ぶ", crt)
+        let mut rng = rand::thread_rng();
+        println!(
+            "||| The Book Of {} |||\n{}\n{}", first_letter_to_uppercase(crt.name), crt.version,
+            PHRASES[rng.gen_range(0,2)]
+        )
     } else {
-        println!("Seems like you've lost your way, gakusei, try again.");
+        println!("Seems like you've lost your way, 学生, try again.");
+    }
+}
+
+/// Converts the first letter of a crate's name to upper case.
+fn first_letter_to_uppercase(c: String) -> String {
+    match c.chars().next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + &c[1..],
     }
 }

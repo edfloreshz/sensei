aeffdd7920748c613f83b00cdc0d5fc0b98b5ef8
Eduardo Flores <edfloreshz@gmail.com>
2020-12-04 09:32:49 -0700

    Refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,122 +17,6 @@ struct CrateInfo {
     warning: String,
 }
 
-impl CrateInfo {
-    /// Creates an object of type CrateInfo.
-    fn new(name: String) -> CrateInfo {
-        CrateInfo {
-            name,
-            version: String::new(),
-            query: String::new(),
-            url: String::new(),
-            warning: String::new(),
-        }
-    }
-    /// Assigns arguments to the structure.
-    fn get_args(&mut self, matches: ArgMatches) {
-        if matches.is_present("local") {
-            if matches.is_present("version") || matches.is_present("query") {
-                self.warning = "Versioning and querying is not available with local crates.".into();
-            }
-            if self.is_local() {
-                self.open_locally()
-            }
-        }
-        if matches.is_present("version") {
-            self.version = matches.value_of("version").unwrap().parse().unwrap();
-        }
-        if matches.is_present("query") {
-            self.query = matches.value_of("query").unwrap().parse().unwrap();
-        }
-    }
-    /// Constructs the url.
-    fn construct_url(&mut self) {
-        if self.query.is_empty() && self.version.is_empty() {
-            self.url = format!("https://docs.rs/{}", self.name);
-        } else {
-            self.url = if self.is_std() {
-                self.url = "https://doc.rust-lang.org".into();
-                let path = "std/index.html";
-                if !self.version.is_empty() && !self.query.is_empty() {
-                    format!("{}/{}/{}?search={}", self.url, self.version, path, self.query)
-                } else if !self.version.is_empty() {
-                    format!("{}/{}/{}", self.url, self.version, path)
-                } else {
-                    format!("{}/{}?search={}", self.url, path, self.query)
-                }
-            } else {
-                self.url = "https://docs.rs".into();
-                if !self.version.is_empty() && !self.query.is_empty() {
-                    format!("{}/{}/{}/{}/?search={}", self.url, self.name, self.version, self.name, self.query)
-                } else if !self.version.is_empty() {
-                    format!("{}/{}/{}/{}", self.url, self.name, self.version, self.name)
-                } else {
-                    format!("{}/{}/?search={}", self.url, self.name, self.query)
-                }
-            }
-        }
-    }
-    /// Checks if the crate is The Standard Library.
-    fn is_std(&self) -> bool {
-        self.name == "std"
-    }
-    /// Checks if the crate is available locally.
-    fn is_local(&self) -> bool {
-        Path::exists(
-            format!(
-                "{}/target/doc/{}/index.html",
-                env::current_dir().unwrap().to_str().unwrap(),
-                self.name
-            )
-            .as_ref(),
-        )
-    }
-    /// Opens the crate's documentation.
-    fn open(&self) {
-        if !(webbrowser::open(&*self.url).is_ok()) {
-            println!("Seems like you've lost your way, 学生, try again.");
-        } else {
-            let mut rng = rand::thread_rng();
-            if self.is_std() {
-                println!(
-                    "||| The Standard Library {}|||\n{}\n{}",
-                    format!("{}", self.version),
-                    self.warning,
-                    PHRASES[rng.gen_range(0, 2)]
-                )
-            } else {
-                println!(
-                    "||| The Book Of {} {}|||\n{}\n{}",
-                    first_letter_to_uppercase(self.name.clone()),
-                    format!("{} ", self.version),
-                    self.warning,
-                    PHRASES[rng.gen_range(0, 2)]
-                )
-            }
-        }
-    }
-    /// Opens the crate's documentation locally.
-    fn open_locally(&self) {
-        if webbrowser::open(&*format!(
-            "{}/target/doc/{}/index.html",
-            env::current_dir().unwrap().to_str().unwrap(),
-            self.name
-        ))
-        .is_ok()
-        {
-            let mut rng = rand::thread_rng();
-            println!(
-                "||| The Book Of {} {}|||\n{}\n{}",
-                first_letter_to_uppercase(self.name.clone()),
-                format!("{} ", self.version),
-                self.warning,
-                PHRASES[rng.gen_range(0, 2)]
-            );
-            exit(0)
-        }
-    }
-}
-
 /// Receives input from the user to process their request.
 ///
 /// # Arguments
@@ -216,5 +100,127 @@ fn first_letter_to_uppercase(c: String) -> String {
     match c.chars().next() {
         None => String::new(),
         Some(f) => f.to_uppercase().collect::<String>() + &c[1..],
+    }
+}
+
+impl CrateInfo {
+    /// Creates an object of type CrateInfo.
+    fn new(name: String) -> CrateInfo {
+        CrateInfo {
+            name,
+            version: String::new(),
+            query: String::new(),
+            url: String::new(),
+            warning: String::new(),
+        }
+    }
+    /// Assigns arguments to the structure.
+    fn get_args(&mut self, matches: ArgMatches) {
+        if matches.is_present("local") {
+            if matches.is_present("version") || matches.is_present("query") {
+                self.warning = "Versioning and querying is not available with local crates.".into();
+            }
+            if self.is_local() {
+                self.open_locally()
+            }
+        }
+        if matches.is_present("version") {
+            self.version = matches.value_of("version").unwrap().parse().unwrap();
+        }
+        if matches.is_present("query") {
+            self.query = matches.value_of("query").unwrap().parse().unwrap();
+        }
+    }
+    /// Constructs the url.
+    fn construct_url(&mut self) {
+        if self.query.is_empty() && self.version.is_empty() {
+            self.url = format!("https://docs.rs/{}", self.name);
+        } else {
+            self.url = if self.is_std() {
+                self.url = "https://doc.rust-lang.org".into();
+                let path = "std/index.html";
+                if !self.version.is_empty() && !self.query.is_empty() {
+                    format!(
+                        "{}/{}/{}?search={}",
+                        self.url, self.version, path, self.query
+                    )
+                } else if !self.version.is_empty() {
+                    format!("{}/{}/{}", self.url, self.version, path)
+                } else {
+                    format!("{}/{}?search={}", self.url, path, self.query)
+                }
+            } else {
+                self.url = "https://docs.rs".into();
+                if !self.version.is_empty() && !self.query.is_empty() {
+                    format!(
+                        "{}/{}/{}/{}/?search={}",
+                        self.url, self.name, self.version, self.name, self.query
+                    )
+                } else if !self.version.is_empty() {
+                    format!("{}/{}/{}/{}", self.url, self.name, self.version, self.name)
+                } else {
+                    format!("{}/{}/?search={}", self.url, self.name, self.query)
+                }
+            }
+        }
+    }
+    /// Checks if the crate is The Standard Library.
+    fn is_std(&self) -> bool {
+        self.name == "std"
+    }
+    /// Checks if the crate is available locally.
+    fn is_local(&self) -> bool {
+        Path::exists(
+            format!(
+                "{}/target/doc/{}/index.html",
+                env::current_dir().unwrap().to_str().unwrap(),
+                self.name
+            )
+                .as_ref(),
+        )
+    }
+    /// Opens the crate's documentation.
+    fn open(&self) {
+        if !(webbrowser::open(&*self.url).is_ok()) {
+            println!("Seems like you've lost your way, 学生, try again.");
+        } else {
+            let mut rng = rand::thread_rng();
+            if self.is_std() {
+                println!(
+                    "||| The Standard Library {}|||\n{}\n{}",
+                    format!("{}", self.version),
+                    self.warning,
+                    PHRASES[rng.gen_range(0, 2)]
+                )
+            } else {
+                println!(
+                    "||| The Book Of {} {}|||\n{}\n{}",
+                    first_letter_to_uppercase(self.name.clone()),
+                    format!("{} ", self.version),
+                    self.warning,
+                    PHRASES[rng.gen_range(0, 2)]
+                )
+            }
+        }
+    }
+    /// Opens the crate's documentation locally.
+    fn open_locally(&self) {
+        if webbrowser::open(&*format!(
+            "{}/target/doc/{}/index.html",
+            env::current_dir().unwrap().to_str().unwrap(),
+            self.name
+        ))
+            .is_ok()
+        {
+            let mut rng = rand::thread_rng();
+            println!(
+                "||| The Book Of {} {}|||\n{}\n{}",
+                first_letter_to_uppercase(self.name.clone()),
+                format!("{} ", self.version),
+                self.warning,
+                PHRASES[rng.gen_range(0, 2)]
+            );
+            exit(0)
+        }
     }
 }

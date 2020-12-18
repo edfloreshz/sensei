18a29d35314c83e226416ced0ce90d7c8b06bedb
Eduardo Flores <edfloreshz@gmail.com>
2020-12-18 14:45:04 -0700

    v0.2.2

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ fn main() {
 /// Creates an object of type ArgMatches with the structure of the CLI.
 fn make_config() -> ArgMatches<'static> {
     App::new("Sensei")
-        .version("0.2.1")
+        .version("0.2.2")
         .author("Eduardo F. <edfloreshz@gmail.com>")
         .about("Opens the documentation for any crate.")
         .arg(

d586201e1c2070fcc08ed91c349b1213e0343c5d
Eduardo Flores <edfloreshz@gmail.com>
2021-11-26 12:14:12 -0700

    PKGBUILD

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,13 @@
+pkgver="0.2.8.$(git rev-list --count HEAD).$(git rev-parse --short HEAD)"
+pkgname="sensei-$pkgver-amd64.tar.gz"
+echo Building v$pkgver\n
+
 cd ..
 mkdir release
 cargo build --release
 mv target/release/sns release 
 cp LICENSE release/LICENSE
 cp README.md release/README.md
-tar -czvf sensei-0.2.8-amd64.tar.gz release
-mv sensei-0.2.8-amd64.tar.gz release
+tar -czvf $pkgname release
+mv $pkgname release
+rm -rf release/{LICENSE,sns,README.md}
